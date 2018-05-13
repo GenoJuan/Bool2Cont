6b72cdff8fa300ed733f9e8fa967ed6385d00b1b
Juan Arias Del Angel <jariasdelangel@gmail.com>
2018-05-12 23:52:28 -0500

    Update make_plot.r
    Add helpers functions "dec2bin" and "Attr2df".

--- Athaliana_CellCycle_Bool2Cont/make_plot.r
+++ Athaliana_CellCycle_Bool2Cont/make_plot.r
@@ -1,5 +1,49 @@
 source("Bool2Cont.r")
+library(BoolNet)
+library(ggplot2)
+library(gridExtra)
+library(reshape)
 
+#########################################################################################################################
+#########################################################################################################################
+# Developed by: Juan A. Arias Del Angel (PhD Candidate) - Instituto de Ecología, Universidad Nacional Autónoma de México
+# Advisor: Mariana Benítez (PhD) - Instituto de Ecología, Universidad Nacional Autónoma de México
+# Last modificacion: May 12th 2018
+
+# Description: It is shown a comparison between a full Boolean network model and a simplified version of itself.
+# The Boolean network model (14 nodes) corresponds to the cell cycle in Arabidopsis thaliana.
+# The model is taken from Ortíz-Gutierrez et al., 2016 (http://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1004486).
+# The state space converges to a single oscillatory attractor (11 states) under synchronous update scheme.
+# The simplified version (8 nodes) preserves the oscillatory attractor (7 states) under synchronous upodate scheme as well as in a Bool-to-continuous mapping strategy.
+# In the simplified version, the system converges to a complex attractor under asynchronous update scheme.
+# This result is easily obtained in the simplified version in contrast to the full version.
+# Note that the simplified version is obtained through an heuristic search so it is not guaranted that this is the optimal simplified version.
+# The strategy was taken from Naldi et al., 2011 (https://www.sciencedirect.com/science/article/pii/S0304397510005839)
+# The Bool2Cont was performed using the strategy presented in Villarreal et al., 2012 (https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.109.118102)
+#########################################################################################################################
+#########################################################################################################################
+
+dec2bin = function(dec,len){
+    bin = rep(0,len)
+    
+    bin = .C("dec2bin",as.integer(bin),as.integer(dec),as.integer(len),NAOK=TRUE)[[1]]
+    return(bin)
+}
+
+Attr2df = function(x){
+    attrs = data.frame(Attr = c(), Variable = c(), Time = c(), State = c())
+    genes = x$stateInfo$genes
+    for(attr in 1:length(x$attractors)){
+        for(s in 1:length(x$attractors[[attr]]$involvedStates)){
+            attr_info = data.frame(Attr = rep(attr, length(genes)), Variable = genes, Time = rep(s, length(genes)), State = dec2bin(x$attractors[[attr]]$involvedStates[s], length(genes)))
+            attrs = rbind.data.frame(attrs, attr_info)
+        }        
+    }
+    return(attrs)
+}
+
+#################################################################################################################
+#################################################################################################################
 net = loadNetwork("Arabidopsis_thaliana_CC.net")
 attr = getAttractors(net)
 attr = Attr2df(attr)
@@ -19,7 +63,7 @@ Boolean_whole = ggplot(attr, aes(x = factor(Time), y = Variable, fill = factor(S
     ggtitle("a) Boolean dynamic for whole A. thaliana cell cycle model") +
     theme(plot.title = element_text(size = 9, face = "bold"))
 
-net = loadNetwork("backup.net")
+net = loadNetwork("Arabidopsis_thaliana_CC_simplified.net")
 attr = getAttractors(net)
 attr = Attr2df(attr)
 
@@ -59,7 +103,7 @@ cont_whole = ggplot(subset(o, variable %in% c("KRP1", "CYCD31", "CYCB11")), aes(
   theme(axis.title = element_text(size=7.5)) #+
   #facet_grid(variable~.)
 
-odeNet = BoolNet2ToContinuous("backup.net")
+odeNet = BoolNet2ToContinuous("Arabidopsis_thaliana_CC_simplified.net")
 times = seq(0, 35, 0.05)
 o = ode(y = state, func = odeNet, parms = parameters, times = times)
 
